797bc87b7a258d03196dcb50d8fd68c015af4b86
Alcrro <alex.roventa94@gmail.com>
2023-06-03 17:54:22 +0300

    main

--- backend/configs/mongoDB.js
+++ backend/configs/mongoDB.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  mongoose.set("strictQuery", false);
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  try {
+    mongoose.set("strictQuery", false);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-  console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    throw error;
+  }
 };
 
 module.exports = connectDB;
